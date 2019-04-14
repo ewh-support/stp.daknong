ccc43c15bf5331eb8858308ba8df6632acaaf294
daknong <daknong@email.com>
2019-04-14 17:26:42 +0700

    Sun 04/14/2019 17:26:32.19

---
+++ web/20180312211947js_/http/stp.daknong.gov.vn/Pages/js/jquery-1.4.1.js
@@ -0,0 +1,22 @@
+
+/*
+     FILE ARCHIVED ON 03:31:23 Jul 04, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:51:53 Apr 14, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 45.675 (3)
+  esindex: 0.005
+  captures_list: 61.731
+  CDXLines.iter: 11.364 (3)
+  PetaboxLoader3.datanode: 68.952 (5)
+  exclusion.robots: 0.129
+  exclusion.robots.policy: 0.12
+  RedisCDXSource: 2.285
+  PetaboxLoader3.resolve: 64.224 (2)
+  load_resource: 176.198
+*/
---
+++ web/20180312211947js_/http/stp.daknong.gov.vn/SiteAssets/SP2010_RibbonToggler.js
@@ -0,0 +1,80 @@
+var ks = "abc";
+function ShowRibbon() 
+{
+   $("#s4-ribbonrow").show();
+   $(".ribbonbackground").show();
+
+}
+
+function HideRibbon() 
+{
+	  $("#s4-ribbonrow").hide();
+      $(".ribbonbackground").hide();
+}
+
+$(document).ready(function()
+{
+	
+   if (typeof _spUserId == "undefined") 
+   {
+      HideRibbon();
+   }
+   else 
+   {
+      ShowRibbon();
+   }
+});
+
+$(document).keydown(function (e) 
+{
+	
+   if (e.keyCode == 17) {if (ks == "b"){ ks = "a" + ks; }else{ ks = "a";} return false; }
+   if (e.keyCode == 16) { if (ks == "a") { ks = ks + "b";}else{ ks = "b";} return false;  }
+   if (e.keyCode == 82) 
+   {
+    	if (ks == "ab") 
+    	{ 
+	         if($("#s4-ribbonrow").css("display")=="none") 
+	         {
+	         	ShowRibbon();
+	         } 
+	         else 
+	         {
+				HideRibbon();
+	         }	         
+	         return false;
+        } 
+   }
+	return true;
+});
+
+
+$(document).keyup(function (e) 
+{
+	 if (e.keyCode == 17 || e.keyCode == 16) { ks = "";}
+
+});
+
+  
+
+/*
+     FILE ARCHIVED ON 02:04:25 Jul 04, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 05:51:42 Apr 14, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 223.66 (3)
+  esindex: 0.008
+  captures_list: 251.858
+  CDXLines.iter: 19.653 (3)
+  PetaboxLoader3.datanode: 49.883 (4)
+  exclusion.robots: 0.169
+  exclusion.robots.policy: 0.158
+  RedisCDXSource: 1.112
+  PetaboxLoader3.resolve: 192.013 (4)
+  load_resource: 30.89
+*/
